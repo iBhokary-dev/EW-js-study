f0e7e568a9314262618c15d8d3b92c1535108e6c
iBhokary-dev <Bhokary404@proton.me>
2024-01-15 16:54:58 -0300

    first lesson

---
+++ chapter-01/demo-01-variables.js
@@ -0,0 +1,24 @@
+/*
+ First stuff, let + declaration = the value given to it
+*/
+let nick = "super man"
+let age = 100
+let power = "fly-out"
+/*
+i can change the value given, whenever i want using let
+*/
+nick = "Batman"
+power = "rich"
+console.log(
+    `Nick: ${nick}
+     Age: ${age}
+     Poder: ${power}`
+)
+
+/*
+    Some reserved-names cant be used as variables declaration
+    https://www.w3schools.com/js/js_reserved.asp
+    let class = 1
+    let function = 2
+*/
+
